6724e24b11f994f8b7d77119980988110969e7c9
jennkao <jennkao92@gmail.com>
2016-11-16 20:10:09 -0800

    Implement most of bare minimum

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,15 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    data: $.extend(options, {key: window.YOUTUBE_API_KEY, maxResults: 5, part: 'snippet', videoEmbeddable: 'true', type: 'video'}),
+    success: (data) => {
+      callback(data.items);
+    },
+    error: function(e) {
+      console.log(e);
+    },
+  });
 };
 
 window.searchYouTube = searchYouTube;
