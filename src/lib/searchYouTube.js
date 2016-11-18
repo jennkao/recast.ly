c244444e76efbf153b01b84958c1fa65539fcc65
jennkao <jennkao92@gmail.com>
2016-11-17 20:12:05 -0800

    Add fetch in place of ajax

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,15 +1,10 @@
 var searchYouTube = (options, callback) => {
-  $.ajax({
-    method: 'GET',
-    url: 'https://www.googleapis.com/youtube/v3/search',
-    data: $.extend(options, {key: window.YOUTUBE_API_KEY, maxResults: 5, part: 'snippet', videoEmbeddable: 'true', type: 'video'}),
-    success: (data) => {
-      callback(data.items);
-    },
-    error: function(e) {
-      console.log(e);
-    },
+  fetch(`https://www.googleapis.com/youtube/v3/search?key=${window.YOUTUBE_API_KEY}&maxResults=5&part=snippet&videoEmbeddable=true&type=video&q=${options.q}`).then((response) => {
+    if (response.ok) {
+      response.json().then((data) => callback(data.items));
+    }
   });
+
 };
 
 window.searchYouTube = searchYouTube;
